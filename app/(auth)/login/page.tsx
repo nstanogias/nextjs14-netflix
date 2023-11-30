fedbc9286a9bfaadc2223c8ed38396460a47fc77
nstanogias <nstanogias@gmail.com>
2023-11-30 17:16:08 +0200

    added navbar

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { GithubIcon } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import GoogleIcon from "../../../public/google.svg";
+import GithubSignInButton from "@/app/components/GithubSignInButton";
+import GoogleSignInButton from "@/app/components/GoogleSignInButton";
+import { authOptions } from "@/app/utils/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const LoginPage: React.FC = () => {
+const LoginPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    return redirect("/home");
+  }
   return (
     <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
-      <form>
+      <form method="post" action="/api/auth/signin">
         <h1 className="text-3xl font-semibold text-white">Log in</h1>
         <div className="space-y-4 mt-5">
           <Input
@@ -36,12 +43,8 @@ const LoginPage: React.FC = () => {
       </div>
 
       <div className="flex w-full justify-center items-center gap-x-3 mt-6">
-        <Button variant="outline" size="icon">
-          <GithubIcon className="w-4 h-4" />
-        </Button>
-        <Button variant="outline" size="icon">
-          <Image src={GoogleIcon} alt="Google Icon" className="w-6 h-6" />
-        </Button>
+        <GithubSignInButton />
+        <GoogleSignInButton />
       </div>
     </div>
   );
---
+++ app/home/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h1>hello from home</h1>;
+}

3f9a2771e5ee4660a1da195495a4a5a9384e11a4
nstanogias <nstanogias@gmail.com>
2023-12-19 09:45:29 +0200

    added pages and server actions

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,12 @@
+import MovieVideo from "../components/MovieVideo";
+import RecentlyAdded from "../components/RecentlyAdded";
+
 export default function HomePage() {
-  return <h1>hello from home</h1>;
+  return (
+    <div className="p-5 lg:p-0">
+      <MovieVideo />
+      <h1 className="text-3xl font-bold ">Recently Added</h1>
+      <RecentlyAdded />
+    </div>
+  );
 }
